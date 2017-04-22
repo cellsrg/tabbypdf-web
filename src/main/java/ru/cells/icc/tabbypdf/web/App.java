4887a95fbc31c1fc65a26964a1950cc0eafe13c5
aaltaev <andalt92@gmail.com>
2017-04-22 12:44:12 +0800

    Added CORS mapping (and configuration for allowed origins)

--- src/main/java/ru/cells/icc/tabbypdf/web/App.java
+++ src/main/java/ru/cells/icc/tabbypdf/web/App.java
@@ -1,9 +1,11 @@
 package ru.cells.icc.tabbypdf.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.MultipartConfigElement;
@@ -13,7 +15,10 @@ import javax.servlet.MultipartConfigElement;
  * @since 0.1
  */
 @SpringBootApplication(scanBasePackages = {"ru.cells.icc.tabbypdf.web"})
-public class App extends WebMvcConfigurerAdapter{
+public class App extends WebMvcConfigurerAdapter {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
@@ -25,5 +30,11 @@ public class App extends WebMvcConfigurerAdapter{
         factory.setMaxFileSize("32MB");
         factory.setMaxRequestSize("32MB");
         return factory.createMultipartConfig();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins);
     }
 }
